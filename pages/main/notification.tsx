4c9572b7d56b522f337be437ed42c4755627cfeb
Sparkcent Technologies <106729949+sparkcent@users.noreply.github.com>
2024-12-11 17:03:48 +0530

    Create notification.tsx

---
+++ pages/main/notification.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, View } from "react-native";
+
+export default function NotificationScreen() {
+    return (
+        <View></View>
+    )
+}
+const styles = StyleSheet.create({
+    
+})
